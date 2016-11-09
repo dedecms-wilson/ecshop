cf0f7ab7570a6b50afd0aaa81749ec9de0a5d3f0
herozeng <herozeng322@gmail.com>
2016-11-09 17:48:48 +0800

    fix static method

--- upload/includes/cls_template.php
+++ upload/includes/cls_template.php
@@ -297,7 +297,7 @@ class cls_template
                  $source= str_replace('%%%SMARTYSP'.$curr_sp.'%%%', '<?php echo \''.str_replace("'", "\'", $sp_match[1][$curr_sp]).'\'; ?>'."\n", $source);
             }
          }
-         return preg_replace("/{([^\}\{\n]*)}/e", "\$this->select('\\1');", $source);
+		 return preg_replace_callback("/{([^\}\{\n]*)}/", function($r) { return $this->select($r[1]); }, $source);
     }
 
     /**
@@ -419,7 +419,10 @@ class cls_template
         }
         else
         {
-            $tag_sel = array_shift(explode(' ', $tag));
+			
+			$tagArr = explode(' ', $tag);
+			$tag_sel = array_shift($tagArr);
+            //$tag_sel = array_shift(explode(' ', $tag));
             switch ($tag_sel)
             {
                 case 'if':
